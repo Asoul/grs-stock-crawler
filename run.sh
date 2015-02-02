b93c12a7d136ea7fabf2b61cf7416b25c6434ff1
asoul <b00902036@ntu.edu.tw>
2015-02-01 23:23:35 -0500

    update to 1/30

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while :
+do
+    pyhon crawl.py
+    #month=`date +%m`
+    #day=`date +%d`
+    #git add .
+    #git commit -m "update to $month/$day"
+    #git push
+    # 24 x 60 x 60 = 86400
+    sleep 80000
+done
